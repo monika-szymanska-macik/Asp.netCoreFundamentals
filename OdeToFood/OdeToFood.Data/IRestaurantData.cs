7e66ae5c932bbc46fcc5a788c00dbadb96c0f62c
Monika <m.szymanska.macik@gmail.com>
2020-12-26 13:55:18 +0100

    Edited data wirh razor pages

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -11,6 +11,10 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
+    }
         public class InMemoryRestaurantData : IRestaurantData
         {
             List<Restaurant> restaurants;
@@ -27,6 +31,27 @@ namespace OdeToFood.Data
             {
                 return restaurants.SingleOrDefault(r => r.Id == id);
             }
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+        public int Commit()
+        {
+            return 0;
+        }
             public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
             {
                 return from r in restaurants
@@ -36,5 +61,4 @@ namespace OdeToFood.Data
                 
             }
         }
-    }
 }
--- OdeToFood/WebApplication1/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/WebApplication1/Pages/Restaurants/Edit.cshtml.cs
@@ -4,29 +4,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
+using static OdeToFood.Core.Restaurant;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+        [BindProperty]
         public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
 
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
-            Restaurant = restaurantData.GetById(restaurantId);
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
             if (Restaurant == null)
             {
                 return RedirectToPage("./NotFounfd");
             }
             return Page();
+        }    
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
+
+            }
+            if(Restaurant.Id > 0)
+            {
+                restaurantData.Update(Restaurant);
+            }
+            else
+            {
+
+                restaurantData.Add(Restaurant);
+            }
+            restaurantData.Commit();
+            TempData["Message"] = "Restaurant saved!";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+
         }
     }
 }
