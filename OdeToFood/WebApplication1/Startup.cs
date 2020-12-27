9ae205d2868d511350dd977171e1fa2ae6d52622
Monika <m.szymanska.macik@gmail.com>
2020-12-27 15:25:23 +0100

    integrated client side

--- OdeToFood/WebApplication1/Startup.cs
+++ OdeToFood/WebApplication1/Startup.cs
@@ -48,9 +48,10 @@ namespace WebApplication1
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseNodeModules();
 
             app.UseRouting();
 
